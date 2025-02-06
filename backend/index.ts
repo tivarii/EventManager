2fe5805dfe2d2ce69b882fce0eb1b5022824f6b6
tivarii <adarshtiwati3576@gmail.com>
2025-02-06 19:07:04 +0530

    for deploying on vercel

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,8 @@
 import ExpressApp from "./src/server/app";
 import { miscConfig } from "./src/config";
 
-const app = ExpressApp();
+export const app = ExpressApp();
 
-app.listen(miscConfig.port, async () => {
-  console.log(`server started at ${miscConfig.port}`);
-});
+// app.listen(miscConfig.port, async () => {
+//   console.log(`server started at ${miscConfig.port}`);
+// });
