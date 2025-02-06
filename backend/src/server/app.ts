206081743437d61c65fa0ae5ea7339f512bd75ef
tivarii <adarshtiwati3576@gmail.com>
2025-02-06 20:26:55 +0530

    dep err res

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -9,13 +9,38 @@ import { errorHandler } from "../middlewares/errorHandler";
 import cors from "cors";
 import { error } from "console";
 
-  export const app = express();
-  app.use(cors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-    credentials: true
-  }));
+export default function ExpressApp(): Application {
+  const app: Application = express();
+  const allowedOrigins = [
+    "http://localhost:5173", // Local development
+    // have to add  // Production
+    "http://localhost:3000", 
+    // // Another allowed origin
+    "https://event-manager-frontend-three.vercel.app",
+
+    "https://eventmanager-omega.vercel.app",
+    "https://event-manager-front-git-bca4a6-adarsh-tiwaris-projects-00965c6b.vercel.app",
+    "https://vercel.com/adarsh-tiwaris-projects-00965c6b/event-manager"
+  ];
+
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        // Allow requests with no origin (like mobile apps, Postman, etc.)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          // If the origin is in the allowedOrigins list, allow it
+          callback(null, true);
+        } else {
+          // If the origin is not in the allowedOrigins list, reject it
+          console.log(origin);
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true, // Allow credentials
+    }),
+  );
   app.use(express.json({ limit: "3mb" }));
 
   app.use("/auth", authRoutes);
@@ -30,4 +55,5 @@ import { error } from "console";
 
   app.use(errorHandler);
 
-
+  return app;
+}
