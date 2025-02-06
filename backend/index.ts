de107a8d7a245829e9efb4c650c442c6592923c0
tivarii <adarshtiwati3576@gmail.com>
2025-02-06 20:40:33 +0530

    dep err res

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
-
-import { app } from "./src/server/app";
+import ExpressApp from "./src/server/app";
 import { miscConfig } from "./src/config";
 
+const app = ExpressApp();
 export default app;
 // app.listen(miscConfig.port, async () => {
 //   console.log(`server started at ${miscConfig.port}`);
