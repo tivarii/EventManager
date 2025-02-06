bad3e77c4802538bbd4ce6a4c02c9fa343391ca5
tivarii <adarshtiwati3576@gmail.com>
2025-02-06 20:16:43 +0530

    dep er re

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
-import ExpressApp from "./src/server/app";
+
+import { app } from "./src/server/app";
 import { miscConfig } from "./src/config";
 
-const app = ExpressApp();
 export default app;
 // app.listen(miscConfig.port, async () => {
 //   console.log(`server started at ${miscConfig.port}`);
--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -9,39 +9,13 @@ import { errorHandler } from "../middlewares/errorHandler";
 import cors from "cors";
 import { error } from "console";
 
-export default function ExpressApp(): Application {
-  const app: Application = express();
-  // const allowedOrigins = [
-  //   "http://localhost:5173", // Local development
-  //   // have to add  // Production
-  //   "http://localhost:3000", 
-  //   // // Another allowed origin
-  //   "https://event-manager-frontend-three.vercel.app",
-
-  //   "https://eventmanager-omega.vercel.app",
-  //   "https://event-manager-front-git-bca4a6-adarsh-tiwaris-projects-00965c6b.vercel.app"
-  // ];
-
-  // app.use(
-  //   cors({
-  //     origin: (origin, callback) => {
-  //       // Allow requests with no origin (like mobile apps, Postman, etc.)
-  //       if (!origin) return callback(null, true);
-
-  //       if (allowedOrigins.includes(origin)) {
-  //         // If the origin is in the allowedOrigins list, allow it
-  //         callback(null, true);
-  //       } else {
-  //         // If the origin is not in the allowedOrigins list, reject it
-  //         console.log(origin);
-  //         callback(new Error("Not allowed by CORS"));
-  //       }
-  //     },
-  //     credentials: true, // Allow credentials
-  //   }),
-  // );
-
-  app.use(cors());
+  export const app = express();
+  app.use(cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    credentials: true
+  }));
   app.use(express.json({ limit: "3mb" }));
 
   app.use("/auth", authRoutes);
@@ -56,5 +30,4 @@ export default function ExpressApp(): Application {
 
   app.use(errorHandler);
 
-  return app;
-}
+
