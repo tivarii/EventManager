5e913e42993dce2b3752e180320e65a430f84ebf
tivarii <adarshtiwati3576@gmail.com>
2025-02-08 12:13:00 +0530

    err res

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
-import ExpressApp from "./src/server/app";
-import { miscConfig } from "./src/config";
+import ExpressApp from "./server/app";
+import { miscConfig } from "./config";
+
 
 const app = ExpressApp();
 export default app;
-app.listen(miscConfig.port, async () => {
-  console.log(`server started at ${miscConfig.port}`);
-});
+// app.listen(miscConfig.port, async () => {
+//   console.log(`server started at ${miscConfig.port}`);
+// });
